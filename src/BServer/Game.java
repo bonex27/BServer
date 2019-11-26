5b34b3816f3f4f8f268be419f047490a739d231f
Pietro Bonechi <p.bonechi1@gmail.com>
2019-11-26 19:27:19 +0100

    Fix mancata log

--- src/BServer/Game.java
+++ src/BServer/Game.java
@@ -17,7 +17,7 @@ import java.util.logging.Logger;
  *
  * @author Bonex & Admir M.
  */
-public class Game implements Runnable{
+public class Game implements Runnable {
 
     private String sName;
     Socket socket;
@@ -28,279 +28,254 @@ public class Game implements Runnable{
     private Box refOpponent[][];
     String comando;
     String sChk;
-    int boatState=0;
+    String f;
     String[] arrOfStr;
-    
-    public Game(Socket socket, String sName,Box refGrid[][],Box refOpponent[][],ArrayList<Boat> Boats) 
-    {
+    int boatState;
+    String s;
+
+    public Game(Socket socket, String sName, Box refGrid[][], Box refOpponent[][], ArrayList<Boat> Boats) {
         this.socket = socket;
         this.sName = sName;
         this.refGrid = refGrid;
         this.refOpponent = refOpponent;
-        this.Boats = (ArrayList)Boats.clone();//Copia delle barche pronte a esssere inserite
+        this.Boats = (ArrayList) Boats.clone();//Copia delle barche pronte a esssere inserite
     }
 
     @Override
-    public void run() 
-    {
+    public void run() {
         try {
             input = new Scanner(socket.getInputStream());
-            output = new PrintWriter(socket.getOutputStream(),true);
+            output = new PrintWriter(socket.getOutputStream(), true);
             System.out.println(sName);
             setup();
             play();
-        }                 
-        catch (IOException ex) {
+        } catch (IOException ex) {
             Logger.getLogger(Game.class.getName()).log(Level.SEVERE, null, ex);
 //        } catch (InterruptedException ex) {
 //            Logger.getLogger(Game.class.getName()).log(Level.SEVERE, null, ex);
 //        }
+        }
     }
-    }
-    private void play() 
-    {
-        try
-        {
-            do
-            {
+
+    private void play() {
+        try {
+            do {
                 //Thread.sleep(100);
 
-                if(sName.equals("1"))
+                if (sName.equals("1")) {
                     NavalBattleServer.turnPlay.acquire();
-                
-                else
+
+                } else {
+
                     NavalBattleServer.turnPlay2.acquire();
-                if(NavalBattleServer.state.equals("stop"))
+
+                }
+                if (NavalBattleServer.state.equals("stop")) {
                     output.println("lose");
-                else
-                {
-                    
-                output.println(this.sName+"@a");
-                
-                comando = input.nextLine();
-                arrOfStr= comando.split("@", 10);
-                sChk = this.attackBoat(Integer.parseInt(arrOfStr[2]),Integer.parseInt(arrOfStr[3]),refOpponent);      
-                output.println(sChk); 
-               
-                                  
-                if(sName.equals("1"))                   
-                    NavalBattleServer.turnPlay2.release();
-                
-                else                   
-                    NavalBattleServer.turnPlay.release();
+                } 
+                else {
+//                    if (true == NavalBattleServer.posi) {
+//
+//                        s = NavalBattleServer.x + "@" + NavalBattleServer.y + "@" + NavalBattleServer.Posbarca;
+//                        output.println(this.sName + "@a" + "@" + s);
+////                        NavalBattleServer.control1=true;
+//
+//                    } //                     else if(true ==NavalBattleServer.control1)
+//                    //                     {
+//                    //                         s= NavalBattleServer.x+"@"+ NavalBattleServer.y+ "@"+ NavalBattleServer.Posbarca;
+//                    //                         output.println(this.sName+ "@a"+"@"+s);
+//                    //                          NavalBattleServer.posi=false;
+//                    //                     }
+//                    else {
+                      output.println(this.sName + "@a");
+//                    }
+
+                    comando = input.nextLine();
+                    arrOfStr = comando.split("@", 10);
+                    sChk = this.attackBoat(Integer.parseInt(arrOfStr[2]), Integer.parseInt(arrOfStr[3]), refOpponent);
+
+//                        NavalBattleServer.x = Integer.parseInt(arrOfStr[2]);
+//                        NavalBattleServer.y = Integer.parseInt(arrOfStr[3]);
+//                        NavalBattleServer.Posbarca = sChk;
+                    output.println(sChk);
+
+                    if (sName.equals("1")) {
+//                        NavalBattleServer.x = Integer.parseInt(arrOfStr[2]);
+                        NavalBattleServer.y = Integer.parseInt(arrOfStr[3]);
+                        NavalBattleServer.Posbarca = sChk;
+                        NavalBattleServer.turnPlay2.release();
+                        NavalBattleServer.posi = true;
+                    } else //                    {if (NavalBattleServer.control1 == true) {
+                    //                            NavalBattleServer.posi = true;
+                    //                            NavalBattleServer.control1 =false;
+                    //                        }
+                    {
+                        NavalBattleServer.y = Integer.parseInt(arrOfStr[3]);
+                        NavalBattleServer.Posbarca = sChk;
+                        NavalBattleServer.turnPlay2.release();
+                        NavalBattleServer.posi = true;
+                        NavalBattleServer.turnPlay.release();
+                    }
+
                 }
-            }while(!NavalBattleServer.state.equals("stop"));
+
+            } while (!NavalBattleServer.state.equals("stop"));
+        } catch (Exception e) {
+            System.out.println("errore attacco " + e);
         }
-        catch(Exception e)
-        {
-            System.out.println("errore attacco "+ e);
-        }
-            
+
     }
-    private void showMatrix()
-    {
-        
-            String a = "";
-            for(int i = 0; i<21;i++){
-                for(int j = 0; j<21;j++){
-                    a += refGrid[j][i].contenuto+"|";
-                }
-                a+="#";
+
+    private void showMatrix() {
+
+        String a = "";
+        for (int i = 0; i < 21; i++) {
+            for (int j = 0; j < 21; j++) {
+                a += refGrid[j][i].contenuto + "|";
             }
-            output.println(a);
+            a += "#";
+        }
+        output.println(a);
     }
-    public String checkBoat(Box refOpponent[][],int x, int y)
-    {
-        int iCounter =0 ;
+
+    public String checkBoat(Box refOpponent[][], int x, int y) {
+        int iCounter = 0;
         int iLung = 0;
         int statBoat;
-        for(int i = 0; i < Boats.size();i++)
-            if(Boats.get(i).getNome().equals(refOpponent[x][y].getNomeBarca()))
+        for (int i = 0; i < Boats.size(); i++) {
+            if (Boats.get(i).getNome().equals(refOpponent[x][y].getNomeBarca())) {
                 iLung = Boats.get(i).iLunghezza;
-            
+            }
+        }
 
-
-        for(int i = 0;i<21;i++)
-            for(int j = 0; j < 21; j++)            
-                if(refOpponent[i][j].getNomeBarca()!= null)                
-                    if(refOpponent[i][j].getNomeBarca().equals(refOpponent[x][y].getNomeBarca())   && refOpponent[i][j].getContenuto() == 'c')
-                    {
+        for (int i = 0; i < 21; i++) {
+            for (int j = 0; j < 21; j++) {
+                if (refOpponent[i][j].getNomeBarca() != null) {
+                    if (refOpponent[i][j].getNomeBarca().equals(refOpponent[x][y].getNomeBarca()) && refOpponent[i][j].getContenuto() == 'c') {
                         iCounter++;
-                    }              
-            
-        
-        
-        if(iCounter == 3)
-        {
+                    }
+                }
+            }
+        }
+
+        if (iCounter == 3) {
             this.boatState++;
-            if(boatState == Boats.size())
-            {
+            if (boatState == Boats.size()) {
                 NavalBattleServer.state = "stop";
                 return "win";
             }
             return "d";
+        } else {
+            return "c";
         }
-            
-        else
-            return"c";
     }
-    
-//        private String setBoat(int x,int y)
-//        {
-//            if(this.checkSpazio(x, y))
-//            {
-//           refGrid[x][y].contenuto='b';
-//            return "ADD";
-//            }
-//        return "NEAR";
-//        
-//        }
-    private String setBoat(int l,char a,int x,int y)
-    {
-        int j=0;
-        if(this.checkSpazio(x, y,l, a) == true)
-               {
-            if(a=='v')
-            {
-                for(int i = y; i < y+l;i++)
-                   {
-                       refGrid[x][i].contenuto = 'b';
-//                       refGrid[x][i].nomeBarca= boatName;;
-//                       this.Boats.get(j).bPosizione.add(refGrid[x][i]);
 
-                   }
+    private String setBoat(int l, char a, int x, int y) {
+        int j = 0;
+        if (this.checkSpazio(x, y, l, a) == true) {
+            if (a == 'v') {
+                for (int i = y; i < y + l; i++) {
+                    refGrid[x][i].contenuto = 'b';
+
+                }
+                return "ADD";
+            } else if (a == 'o') {
+                for (int i = x; i < x + l; i++) {
+                    refGrid[i][y].contenuto = 'b';
+                }
                 return "ADD";
             }
-            else if(a=='o')
-            {
-                for(int i = x; i < x+l;i++)
-                   {
-                        refGrid[i][y].contenuto = 'b';
-//                        refGrid[i][y].nomeBarca= boatName;
-//                        this.Boats.get(j).bPosizione.add(refGrid[i][y]);
-                   }
-                return "ADD";
-            }           
         }
         return "NEAR";
     }
-    public boolean checkSpazio(int x,int y,int iLung,char cOr )//Controlla in tutte le 8 caselle vicine 
-    {
-        
-        
-      if(cOr == 'o')
-        {
-            int iX;
-           
-            for(iX = x; iX < x+iLung;iX++){
-              
-                    if(refGrid[iX][y].contenuto == 'b')
-                        return false;
-                
-            }
-            if(refGrid[x][y-1].contenuto == 'b')
-                        return false;
-            if(refGrid[x+iLung][y].contenuto == 'b')
-                        return false;
-        return true;
-        }
-        else if(cOr == 'v')
-        {
-           
-            int iY;
-            for(iY = y; iY < y+iLung;iY++){
-               
-                    if(refGrid[x][iY].contenuto == 'b')
-                        return false;
-               
-            }
-            if(refGrid[x-1][y].contenuto == 'b')
-                        return false;
-            if(refGrid[x][y+iLung].contenuto == 'b')
-                        return false;
-        return true;
-        }       
-        return false;
-    }
-    
-    public boolean checkSpazio2(int x,int y,int iLung,char orientation )//Controlla in tutte le 8 caselle vicine 
-    {
-        switch(orientation){
-            case 'v':
-                for(int X = x-1; X < x+2; X++){
-                    if(x==0) X++;
-                    if(X==21) X++;
-                    for(int Y = y-1; Y <= y+iLung; Y++){
-                        if(y==0) Y++;
-                        if(y+iLung==21) Y++;
-                        if(refGrid[X][Y].contenuto == 'b')
-                            return false;
-                    }
-                }                
-                return true;
-                
-            case 'o':
-                for(int Y = y-1; Y < y+2; Y++){
-                    if(y==0) Y++;
-                    if(Y==21) Y++;
-                    for(int X = x-1; X <= x+iLung; X++){
-                        if(x==0) X++;
-                        if(x+iLung==21) X++;
-                        if(refGrid[X][Y].contenuto == 'b')
-                            return false;
-                    }
-                }  
-                return true;
-        }
-        return false;
-    }
-    public String attackBoat(int x,int y,Box refOpponent[][])
-    {
-        
-        if(refOpponent[x][y].contenuto=='b')
-        {
-            refOpponent[x][y].contenuto='c';
-           // NavalBattleServer.pos+=x+"@"+y;
-            return checkBoat( refOpponent, x, y);
-        }
-         else if(refOpponent[x][y].contenuto=='c')
-         {
-                return "gc";//Gia colpita
-          }
-        else
-         {
-            return "m";
-         }
-    
-        
-    }
-  
-    public void setup()
-    {
-        int f=0;
-            try{
-                for(int i = 0; i < Boats.size();i++){
-                    
-                    output.println(this.sName+"@p");//client
-                   
-                    do{
-                        output.println(Boats.get(i).iLunghezza+"@"+Boats.get(i).nome);
-                        comando = input.nextLine();
-                        arrOfStr= comando.split("@", 10);
-                        sChk = this.setBoat(Integer.parseInt(arrOfStr[0]),arrOfStr[1].charAt(0),Integer.parseInt(arrOfStr[2]),Integer.parseInt(arrOfStr[3]));
-                        output.println(sChk);
-                    }while(sChk.equals("NEAR") );
-                    
-                }
-                output.println(this.sName+"@v");
-                if(input.nextLine().equals("stampa"))
-                    showMatrix();
-            }
-            catch(Exception e){
-                System.out.println(e+"ci");
-            }
-    }
-    //controllo il turno del giocatore e se hai un avversario
-}
-    
 
+    public boolean checkSpazio(int x, int y, int iLung, char orientation)//Controlla in tutte le 8 caselle vicine 
+    {
+        switch (orientation) {
+            case 'v':
+                for (int X = x - 1; X < x + 2; X++) {
+                    if (x == 0) {
+                        X++;
+                    }
+                    if (X == 21) {
+                        X++;
+                    }
+                    for (int Y = y - 1; Y <= y + iLung; Y++) {
+                        if (y == 0) {
+                            Y++;
+                        }
+                        if (y + iLung == 21) {
+                            Y++;
+                        }
+                        if (refGrid[X][Y].contenuto == 'b') {
+                            return false;
+                        }
+                    }
+                }
+                return true;
+
+            case 'o':
+                for (int Y = y - 1; Y < y + 2; Y++) {
+                    if (y == 0) {
+                        Y++;
+                    }
+                    if (Y == 21) {
+                        Y++;
+                    }
+                    for (int X = x - 1; X <= x + iLung; X++) {
+                        if (x == 0) {
+                            X++;
+                        }
+                        if (x + iLung == 21) {
+                            X++;
+                        }
+                        if (refGrid[X][Y].contenuto == 'b') {
+                            return false;
+                        }
+                    }
+                }
+                return true;
+        }
+        return false;
+    }
+
+    public String attackBoat(int x, int y, Box refOpponent[][]) {
+
+        if (refOpponent[x][y].contenuto == 'b') {
+            refOpponent[x][y].contenuto = 'c';
+            // NavalBattleServer.pos+=x+"@"+y;
+            return checkBoat(refOpponent, x, y);
+        } else if (refOpponent[x][y].contenuto == 'c') {
+            return "gc";//Gia colpita
+        } else {
+            return "m";
+        }
+
+    }
+
+    public void setup() {
+        int f = 0;
+        try {
+            for (int i = 0; i < Boats.size(); i++) {
+
+                output.println(this.sName + "@p");//client
+
+                do {
+                    output.println(Boats.get(i).iLunghezza + "@" + Boats.get(i).nome);
+                    comando = input.nextLine();
+                    arrOfStr = comando.split("@", 10);
+                    sChk = this.setBoat(Integer.parseInt(arrOfStr[0]), arrOfStr[1].charAt(0), Integer.parseInt(arrOfStr[2]), Integer.parseInt(arrOfStr[3]));
+                    output.println(sChk);
+                } while (sChk.equals("NEAR"));
+
+            }
+            output.println(this.sName + "@v");
+            if (input.nextLine().equals("stampa")) {
+                showMatrix();
+            }
+        } catch (Exception e) {
+            System.out.println(e + "ci");
+        }
+    }
+
+}
--- src/BServer/NavalBattleServer.java
+++ src/BServer/NavalBattleServer.java
@@ -16,66 +16,60 @@ import java.util.concurrent.Semaphore;
  *
  * @author Bonex & Admir M.
  */
-public class NavalBattleServer { 
+public class NavalBattleServer {
+
     /**
      * @param args
      */
     static Semaphore turnPlay = new Semaphore(1);
     static Semaphore turnPlay2 = new Semaphore(0);
-    static String state="go";
+    static String state = "go", Posbarca;
     static Box bPlayerOne[][] = new Box[21][21];
-    static Box bPlayerTwo[][] = new Box[21][21]; 
+    static Box bPlayerTwo[][] = new Box[21][21];
     static ArrayList<Boat> Boats = new ArrayList<Boat>();//Riempito e clonato per ogni giocatore
-    static String pos;
-    public static void main(String[] args) throws IOException 
-    {
-           initMatrix (bPlayerOne);//Inizializzazione matrice a m
-                initMatrix (bPlayerTwo);
-        
-        
-        try{
-            Boat b1 = new Boat(1,"o");
+    static boolean posi = false, control1 = true;
+    static int x, y, t = 0;
+
+    public static void main(String[] args) throws IOException {
+        initMatrix(bPlayerOne);//Inizializzazione matrice a m
+        initMatrix(bPlayerTwo);
+
+        try {
+            Boat b1 = new Boat(1, "o");
             Boats.add(b1);
-            Boat b2 = new Boat(2,"v");
-           Boats.add(b2);
-            Boat b3 = new Boat(3,"v");
+            Boat b2 = new Boat(2, "v");
+            Boats.add(b2);
+            Boat b3 = new Boat(3, "v");
             Boats.add(b3);
-            Boat b4 = new Boat(3,"o");
-            Boats.add(b4);
-            Boat b5 = new Boat(3,"o");
-            Boats.add(b5);
-            Boat b6 = new Boat(4,"v");
-            Boats.add(b6);
-            Boat b7 = new Boat(5,"o");
-            Boats.add(b7);
-                
+//            Boat b4 = new Boat(3,"o");
+//            Boats.add(b4);
+//            Boat b5 = new Boat(3,"o");
+//            Boats.add(b5);
+////            Boat b6 = new Boat(4,"v");
+//            Boats.add(b6);
+//            Boat b7 = new Boat(5,"o");
+//            Boats.add(b7);
+
             ServerSocket server = new ServerSocket(6012);
             System.out.println("BServer is onine!");
             ExecutorService ListaConnessioni = Executors.newFixedThreadPool(2);
-            while(true)
-            {
-             
-                ListaConnessioni.execute(new Game(server.accept(),"1",bPlayerOne,bPlayerTwo,Boats));              
-                ListaConnessioni.execute(new Game(server.accept(),"2",bPlayerTwo,bPlayerOne,Boats));
+            while (true) {
+
+                ListaConnessioni.execute(new Game(server.accept(), "1", bPlayerOne, bPlayerTwo, Boats));
+                ListaConnessioni.execute(new Game(server.accept(), "2", bPlayerTwo, bPlayerOne, Boats));
             }
+        } catch (Exception e) {
+            System.out.println("Errore server" + e);
         }
-        catch(Exception e)
-        {
-            System.out.println("Errore server"+ e);
-        }
-        
-        
+
     }
-        static void initMatrix(Box a[][])
-        {
-           for(int i = 0; i<21;i++)
-         {
-             for(int j = 0; j<21;j++)
-            {
-             a[i][j] = new Box();
+
+    static void initMatrix(Box a[][]) {
+        for (int i = 0; i < 21; i++) {
+            for (int j = 0; j < 21; j++) {
+                a[i][j] = new Box();
             }
-         } 
         }
-    
-    
+    }
+
 }
